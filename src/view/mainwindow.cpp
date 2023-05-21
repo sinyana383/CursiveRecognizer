9ce0fc83b56d4e9b5cc5f91a72278321ce015932
ddurrand <ddurrand@student.21-school.ru>
2023-05-21 18:39:52 +0300

    layouts added

--- src/view/mainwindow.cpp
+++ src/view/mainwindow.cpp
@@ -3,18 +3,18 @@
 
 void MainWindow::handleButton()
 {
-    if (!(ui->widget->Pixels_draw.empty()) && ui->radioButton->isChecked())
+    if (!(ui->paintWidjet->Pixels_draw.empty()) && ui->radioButton->isChecked())
     {
-        //QMessageBox::about(this,"test output", QString::number(ui->widget->Pixels[1]));//здесь нужно вызвать функцию распознавания
-        ui->label->setText("This is A");
-        ui->widget->Pixels_draw.clear();
+        //QMessageBox::about(this,"test output", QString::number(ui->paintWidjet->Pixels[1]));//здесь нужно вызвать функцию распознавания
+        ui->prediction->setText("This is A");
+        ui->paintWidjet->Pixels_draw.clear();
     }
 
-    if (!(ui->widget->Pixels_file.empty()) && ui->radioButton_2->isChecked())
+    if (!(ui->paintWidjet->Pixels_file.empty()) && ui->radioButton_2->isChecked())
     {
-        //QMessageBox::about(this,"test output", QString::number(ui->widget->Pixels[1]));//здесь нужно вызвать функцию распознавания
-        ui->label->setText("This is B");
-        ui->widget->Pixels_file.clear();
+        //QMessageBox::about(this,"test output", QString::number(ui->paintWidjet->Pixels[1]));//здесь нужно вызвать функцию распознавания
+        ui->prediction->setText("This is B");
+        ui->paintWidjet->Pixels_file.clear();
     }
 }
 
@@ -71,7 +71,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->widget_2->setBackground(QColor(190, 251, 188));
     //ui->widget_2->replot();
     //ui->widget_2->setBackground(Color::yellow)
-    ui->gridWidget->hide();
+    ui->trainLayoutWidget->hide();
     ui->label_19->setWordWrap(true);
     ui->commandLinkButton->setIcon(QIcon(":/images/settings-gear.png"));
     //ui->commandLinkButton->setStyleSheet("QCommandLinkButton {color: black;}");
@@ -104,15 +104,15 @@ void MainWindow::bmp_file_dialog() {
                            "Выберите квадратное изображение .bmp не меньше "
                            "28x28 и не больше 512x512");
     } else {
-        ui->widget->Pixels_file.clear();
-        ui->widget->setEnabled(false);
+        ui->paintWidjet->Pixels_file.clear();
+        ui->paintWidjet->setEnabled(false);
         img = img.scaled(28, 28);
         img = img.transformed(QTransform().rotate(-90));
 
-        ui->widget->Pixels_file.push_back(15);
+        ui->paintWidjet->Pixels_file.push_back(15);
         for (int i = img.size().width() - 1; i >= 0; i--) {
             for (int j = 0; j < img.size().height(); j++) {
-                ui->widget->Pixels_file.push_back(img.pixelColor(j, i).black());
+                ui->paintWidjet->Pixels_file.push_back(img.pixelColor(j, i).black());
             }
         }
     }
@@ -120,13 +120,13 @@ void MainWindow::bmp_file_dialog() {
 
 void MainWindow::selectDrawMode()
 {
-    ui->widget->setEnabled(true);
+    ui->paintWidjet->setEnabled(true);
     ui->pushButton_2->setEnabled(false);
 }
 
 void MainWindow::selectFileMode()
 {
-    ui->widget->setEnabled(false);
+    ui->paintWidjet->setEnabled(false);
     ui->pushButton_2->setEnabled(true);
 }
 
@@ -138,17 +138,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_commandLinkButton_clicked()
 {
-    ui->gridWidget->show();
-    ui->verticalLayoutWidget->hide();
     if (!IsClickedCommandButton)
     {
+        ui->testLayout->hide();
+        ui->trainLayoutWidget->show();
         IsClickedCommandButton = true;
     }
     else
     {
+        ui->trainLayoutWidget->hide();
+        ui->testLayout->show();
         IsClickedCommandButton = false;
-        ui->gridWidget->hide();
-        ui->verticalLayoutWidget->show();
     }
 
 }
@@ -173,14 +173,14 @@ void MainWindow::changeValueOfSlider4()
 
 void MainWindow::on_pushButton_3_clicked()
 {
-    ui->gridWidget->hide();
-    ui->verticalLayoutWidget->show();
+    ui->trainLayoutWidget->hide();
+    ui->trainLayoutWidget->show();
     ui->accuracy->setText("0");
     ui->fmeasure->setText("0");
     ui->recall->setText("0");
     ui->time->setText("0");
     ui->precision->setText("0");
-    ui->gridWidget->hide();
+    ui->trainLayoutWidget->hide();
     IsClickedCommandButton = false;
 }
 
